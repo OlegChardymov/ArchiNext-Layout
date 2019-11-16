3680412a14e7b8809dcdb93790519b1d24b18757
Oleg Chardymov <54996009+OlegChardymov@users.noreply.github.com>
2019-11-16 19:27:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const navSlide = () => {
 
   burgerMenu.addEventListener("click", () => {
     nav.classList.toggle("nav__links--active");
+    burgerMenu.classList.toggle("burger-menu--active");
     burgerMenu.classList.toggle("toggle");
   });
 };
