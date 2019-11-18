2f5784a854177ccfabb3ff9e79fd1fdbda07a480
Oleg Chardymov <54996009+OlegChardymov@users.noreply.github.com>
2019-11-18 11:10:39 +0300

    Closing menu after clicking on nav link

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const navSlide = () => {
-  const burgerMenu = document.querySelector(".burger-menu");
-  const nav = document.querySelector(".nav__links");
+const burgerMenu = document.querySelector(".burger-menu");
+const nav = document.querySelector(".nav__links");
+const links = document.querySelectorAll(".nav__link");
 
+const navSlide = () => {
   burgerMenu.addEventListener("click", () => {
     nav.classList.toggle("nav__links--active");
     burgerMenu.classList.toggle("burger-menu--active");
@@ -9,4 +10,19 @@ const navSlide = () => {
   });
 };
 
-navSlide();
+const navClose = () => {
+  links.forEach(function(link) {
+    link.addEventListener("click", () => {
+      nav.classList.remove("nav__links--active");
+      burgerMenu.classList.remove("burger-menu--active");
+      burgerMenu.classList.remove("toggle");
+    });
+  });
+};
+
+const navAnimation = () => {
+  navSlide();
+  navClose();
+};
+
+navAnimation();
